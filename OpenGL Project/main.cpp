02cee73b44cc2f0c7fb1b23651ffde1e600881fc
Alex Fish <iamalexfish@gmail.com>
2024-11-15 16:28:20 -0600

    RGB

--- OpenGL Project/main.cpp
+++ OpenGL Project/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <math.h>
 #include "Matrix.h"
 
 int screenWidth = 800;
@@ -10,24 +11,31 @@ int screenHeight = 600;
 const char* vertexShaderSource = R"(
 #version 330 core
 layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+out vec3 color;
+uniform float scale;
+
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = vec4(aPos.x * scale, aPos.y * scale, aPos.z * scale, 1.0);
+    color = aColor;
 }    
 )";
 // This code runs on the GPU
 const char* fragmentShaderSource = R"(
 #version 330 core
+in vec3 color;
 out vec4 FragColor;
 void main() {
-    FragColor = vec4(0.8, 0.3, 0.4, 1.0);
+    FragColor = vec4(color, 1.0);
 }    
 )";
 
 GLfloat vertices[] =
 {
-   -0.5f, 0.0f, 0.0f,
-    0.0f, 0.8f, 0.0f,
-    0.5f, 0.0f, 0.0f
+//      Position              RGB
+   -0.5f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
+    0.0f, 0.8f, 0.0f,   0.0f, 1.0f, 0.0f,
+    0.5f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f
 };
 
 GLuint shaderProgram;
@@ -87,7 +95,7 @@ static void SetupBuffers()
                                  GPU Buffer Memory Layout
                   |----------------------------------------------------|
                   |       Position         U,V           Normal        |
-      Vertex  --->|   0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 2.0f, 2.0f, 2.0f   | |
+      Vertex  --->|   0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 2.0f, 2.0f, 2.0f   |
                   |  |________________||__________||________________|  |
                   |      Attribute       Attribute      Attribute      |
                   |       12 bytes        8 bytes        12 bytes      |
@@ -100,9 +108,12 @@ static void SetupBuffers()
      Param 4. Normalized? If color = 255, must be normalized between 0 and 1. Can be done on CPU or have OpenGL do it for you.
      Param 5. Stride is the amount of bytes between each vertex. Above would be
      Param 6. Offset of the each attribute of one vertex.*/
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (void*)0);
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0); // Attribute index 
+    // RGB
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1); // Attribute index 
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); // 0 = usually means no state
     glBindVertexArray(0); // 0 = usually means no state
@@ -135,6 +146,7 @@ int main()
 
     SetupShaders();
     SetupBuffers();
+    GLuint uniID = glGetUniformLocation(shaderProgram, "scale");
 
     // bottom left to top right
     glViewport(0, 0, screenWidth, screenHeight);
@@ -144,8 +156,11 @@ int main()
         // Prepare to clear screen color and swap buffers to display the changed color.
         glClearColor(0.07f, 0.13f, 0.17f, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
+        static int frame = 0;
+        frame += 1;
         glUseProgram(shaderProgram);
+        glUniform1f(uniID, cos(frame * (3.14159f / 4.0f) * 0.01f));//Must declare after shader program
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3); // (type, starting index, num to draw) example: index 1 = (0,0,0)
         
